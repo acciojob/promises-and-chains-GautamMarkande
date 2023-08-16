519231ae93c320d63856986e9fa13aa68eeb9851
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2023-08-16 18:14:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const submitbtn = document.getElementById("btn");
+let prom1 = new Promise((resolve,reject)=>{
+submitbtn.addEventListener("click", (e)=>{
+	const age = parseInt(document.getElementById("age").value);
+	const name = document.getElementById("name").value;
+	if(age>18){
+		resolve(name);
+	}else{
+		reject(name)
+	}
+	
+})
+})
+prom1.then((name)=>{
+	alert(`Welcome, ${name}. You can vote.`)
+}).catch((name)=>{
+	alert(`Oh sorry ${name}. You aren't old enough.`)
+})
